0478b85bf82a2f7bd2c6c95ae36b5f5ad29d196d
Jan <jan.neumann-1@studium.uni-hamburg.de>
2026-01-21 16:38:41 +0100

    initial deploy script not working

--- whistle_detection/deploy.py
+++ whistle_detection/deploy.py
@@ -1,11 +1,14 @@
 import pyaudio
 import wave
 import time
-import keyboard
+#import keyboard
 import torch
 import torch.nn as nn
 import torchaudio
 from torchvision import models
+import dataset
+import model as pymod
+import numpy as np
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -21,11 +24,14 @@ segment_count = 0
 frames = []
 start_time = time.time()
 
+#TODO cleanup v
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = models.efficientnet_b0()
+#model = models.efficientnet_b0()
 #model.classifier[0] = nn.Dropout(p=0.2) #p=0.2 is default
-in_features = model.classifier[1].in_features
-model.classifier[1] = nn.Linear(in_features, 2)
+#in_features = model.classifier[1].in_features
+#model.classifier[1] = nn.Linear(in_features, 2)
+
+model = pymod.get_model(device)
 
 model.load_state_dict(torch.load(CHECKPOINT_PATH, map_location=device))
 model.to(device)
@@ -33,22 +39,47 @@ model.eval()
 
 while True: 
     data = stream.read(CHUNK, exception_on_overflow=False)
-    frames.append(data)
+    #frames.append(data)
+    audio_np = np.frombuffer(data, dtype=np.int16)
+    
 
+    # int16 → float32 in [-1.0, 1.0]
+    audio_np = audio_np.astype(np.float32) / 32768.0
+    
+
+    # torch tensor
+    audio_tensor = torch.from_numpy(audio_np)
+
+    # torchaudio expects (channels, samples)
+    #audio_tensor = audio_tensor.unsqueeze(0)
+    #print(audio_tensor.shape)
+    mel = dataset.convert_waveform_to_spectogram(SAMPLE_RATE, audio_tensor)
+    #print(mel.shape)
+    mel = mel.unsqueeze(0)
+    #print(torch.isnan(mel).sum(), torch.isinf(mel).sum())
+    #print(mel)
+    with torch.no_grad():
+            output = model(mel).squeeze()
+            print(output)
+
+    """
     if time.time() - start_time > CHUNK_LENGTH_SEK:
         filename = "currenchunk.wav"
         wf = wave.open(filename, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
         wf.setframerate(SAMPLE_RATE)
-        mel = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, n_mels=64)(wf).log2().repeat(3, 1, 1)
+        fnames = wf.writeframes()
+        print(wf)
+        #mel = torchaudio.transforms.MelSpectrogram(SAMPLE_RATE, n_mels=64)(wf).log2().repeat(3, 1, 1)
+        wf = np.array([CHANNELS, data])
+        wavetensor = torch.from_numpy(wf)
+        mel = dataset.convert_waveform_to_spectogram(SAMPLE_RATE, wavetensor)
         with torch.no_grad():
             output = model(mel)
+            print(output)
             #TODO Output verarbeiten
-
-    if keyboard.is_pressed("q"):
-        print("Beende...")
-        break
+    """
 
 stream.stop_stream()
 stream.close()
